a39844ac084b905c1f5d2e367e71bdfbd012df84
Iliyan Ganev <ganeviliyan@gmail.com>
2017-12-02 22:31:02 -0800

    Able to send ajax post request

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -4,12 +4,20 @@ var NewItem = createReactClass({
 		var name = this.nameInput.value;
 		var description = this.descriptionInput.value;
 		console.log(name, description)
+		$.ajax({
+			url: '/api/v1/items',
+			type: 'POST',
+			data: { item: { name: name, description: description}},
+			success: (response) => {
+				console.log('it worked!', response);
+			}
+		});
 	},
 
 	render: function() {
 		return (
 			<div>
-				<input placeholder='Enter name' ref={(input) => { this.nameInput = input;}} />
+				<input placeholder='Enter name' ref={input => { this.nameInput = input;}} />
 				<input placeholder='Enter a desc' ref={(input) => { this.descriptionInput = input;}} />
 				<button onClick={this.handleClick}>Submit</button>
 			</div>
