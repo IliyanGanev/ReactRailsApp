b2602af519312266c86bf8c75a79e867d0ed6d9c
Iliyan Ganev <ganeviliyan@gmail.com>
2017-12-02 22:08:31 -0800

    Fix both refs

--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -1,15 +1,16 @@
 var NewItem = createReactClass({
 	
 	handleClick() {
-		var name = this.refs.name.value;
-		var description = this.refs.description.value;
+		var name = this.nameInput.value;
+		var description = this.descriptionInput.value;
+		console.log(name, description)
 	},
 
 	render: function() {
 		return (
 			<div>
-				<input  placeholder='Enter name' />
-				<input  placeholder='Enter a desc' />
+				<input placeholder='Enter name' ref={(input) => { this.nameInput = input;}} />
+				<input placeholder='Enter a desc' ref={(input) => { this.descriptionInput = input;}} />
 				<button onClick={this.handleClick}>Submit</button>
 			</div>
 		)
